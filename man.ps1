201a62476b0bee49c09ec1787779bfc4915a2862
Brian <brtsuper07@gmail.com>
2022-06-14 19:01:35 +0900

    Modified man.ps1 so it won't leave trails of .txt file everywhere

--- man.ps1
+++ man.ps1
@@ -1,27 +1,25 @@
 #!/usr/bin/env powershell
 
-Get-Help @args > man_text.txt
 $WORD = $args[0]
 
 #colors
 $ORANGE = "$([char]0x1b)[38;2;239;155;64m"
-$YELLOW = "$([char]0x1b)[93m" 
+$YELLOW = "$([char]0x1b)[93m"
 $LGREEN = "$([char]0x1b)[38;2;160;199;75m"
 # $GREEN = "$([char]0x1b)[92m"
 # $RED = "$([char]0x1b)[91m"
 # $CYAN = "$([char]0x1b)[96m"
-# $BLUE = "$([char]0x1b)[94m" 
-# $PURPLE = "$([char]0x1b)[95m" 
+# $BLUE = "$([char]0x1b)[94m"
+# $PURPLE = "$([char]0x1b)[95m"
 $RESET = "$([char]0x1b)[0m"
 
 #elements
-$headings = '^[A-Z \d\W]+$'
+$headings = '(?m)^[A-Z \d\W]+$'
 $cmdlet = '\b'+$WORD+'\b'
 $options = '\B-[a-zA-Z]*\b'
 
-cat man_text.txt | `
-    ForEach-Object {$_ `
-        -replace $options, "$LGREEN`$0$RESET" `
-        -creplace $headings, "$ORANGE`$0$RESET" `
-        -creplace $cmdlet, "$YELLOW`$0$RESET" `
-    }
+Get-Help @args | Out-String | ForEach-Object {$_ `
+  -replace $options, "$LGREEN`$0$RESET" `
+  -creplace $headings, "$ORANGE`$0$RESET" `
+  -creplace $cmdlet, "$YELLOW`$0$RESET" `
+}
