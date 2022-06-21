e02aa54365b5cd4254cc8c49f20db7b5eaabfc2a
Brian <brtsuper07@gmail.com>
2022-06-21 11:47:36 +0900

    Updated version(3) and script

--- man-highlighting.psm1
+++ man-highlighting.psm1
@@ -47,9 +47,9 @@ Function Get-Help {
 
         # The styles to apply
         $style = @{
-            SECTION = $PSStyle.Foreground.FromRgb(239,155,64)
+            SECTION = $PSStyle.Formatting.FormatAccent
             COMMAND = $PSStyle.Foreground.BrightYellow
-            PARAM   = $PSStyle.Foreground.FromRgb(160,199,75)
+            PARAM   = $PSStyle.Foreground.FromRgb(64,200,230)
         }
 
         # Patterns with named capturing groups that match the $style keys
